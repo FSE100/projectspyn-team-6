73799bdde7e01cd41c2aa2f89775a805320e1dde
Justin <justngo1@gmail.com>
2019-10-31 08:54:50 -0700

    Commit #5
    Testing File Changed

--- test.m
+++ test.m
@@ -1,6 +1,7 @@
 % Initial Bluetooth Connection Command
 % brick = ConnectBrick('POE')
 function test()
+brick = ConnectBrick('POE');
 brick.MoveMotor('A', 50);
 pause(5); % pause the script for 5 second
 brick.MoveMotor('B', 50);
